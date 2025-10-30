1b5112237d3d017bd69dc025e22be119c45b3141
wootaepark <pakdal99@gmail.com>
2025-10-30 16:01:09 +0900

    feat(lotto): validate input budget type and handle invalid amount format

---
+++ src/main/java/lotto/utils/InputValidator.java
@@ -0,0 +1,25 @@
+package lotto.utils;
+
+public class InputValidator {
+
+    final static int DIVIDER = 1000;
+
+    public static void validateInputBudget(String budget) {
+        validateIsNumeric(budget);
+        validateIsDivisibleByDivider(Integer.parseInt(budget));
+    }
+
+    private static void validateIsNumeric(String budget) {
+        try {
+            Integer.parseInt(budget);
+        } catch (NumberFormatException e) {
+            throw new IllegalArgumentException("올바르지 않은 형태의 구매 금액 입니다.");
+        }
+    }
+
+    private static void validateIsDivisibleByDivider(int budget) {
+        if (budget % DIVIDER != 0) {
+            throw new IllegalArgumentException("구매 금액은 1000원 단위로 입력해야 합니다.");
+        }
+    }
+}
--- src/main/java/lotto/view/InputVIew.java
+++ src/main/java/lotto/view/InputVIew.java
@@ -1,12 +1,15 @@
 package lotto.view;
 
 import camp.nextstep.edu.missionutils.Console;
+import lotto.utils.InputValidator;
 
 public class InputVIew {
 
     public static int inputBudget() {
         System.out.println("구입 금액을 입력해주세요");
-        return Integer.parseInt(Console.readLine());
+        String budget = Console.readLine();
+        InputValidator.validateInputBudget(budget);
+        return Integer.parseInt(budget);
     }
 
 }
