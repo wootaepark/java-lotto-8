3d4a11eb540c78e62cd0d4bc2c99fda13868ec98
wootaepark <pakdal99@gmail.com>
2025-10-31 20:10:23 +0900

    refactor(view): Reorganize directories for budget input view Move BudgetInputView into subdirectories

--- src/main/java/lotto/controller/LottoController.java
+++ src/main/java/lotto/controller/LottoController.java
@@ -1,14 +1,14 @@
 package lotto.controller;
 
-import lotto.view.BudgetInputVIew;
-import lotto.view.LottoPurchaseCountView;
+import lotto.view.input.BudgetInputVIew;
+import lotto.view.output.LottoPurchaseCountView;
 
 public class LottoController {
 
     public void run() {
         int budget = BudgetInputVIew.inputBudget();
         int purchasedLottoCount = LottoPurchaseCountView.displayLottoPurchaseCount(budget);
-        
+
 
     }
 }
