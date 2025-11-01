c7fbe7eb7744f3ba1ca110d862f786599c5f15f7
wootaepark <pakdal99@gmail.com>
2025-11-01 18:32:51 +0900

    feat(lotto): Add duplicate check for winning numbers (parse as List and convert to Set)

--- src/main/java/lotto/utils/validator/WinningNumberValidator.java
+++ src/main/java/lotto/utils/validator/WinningNumberValidator.java
@@ -3,19 +3,25 @@ package lotto.utils.validator;
 import static lotto.utils.constant.LottoConstants.LOTTO_NUMBER_COUNT;
 
 import java.util.Arrays;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 public class WinningNumberValidator {
 
-    public static List<Integer> validateInputWinningNumber(String input) {
+    public static Set<Integer> validateInputWinningNumber(String input) {
         // 1. ',' 구분 시 번호의 개수가 6개인지 
-        // 2. 각 번호의 범위가 올바른지
-        // 3. 올바른 형태의 input 인지 (, 이외의 다른 문자)
+        // 2. 중복된 숫자가 없는지
+        // 3. 각 번호의 범위가 올바른지
+        // 4. 올바른 형태의 input 인지 (, 이외의 다른 문자)
 
         List<Integer> winningNumbers = parseNumbers(input);
+        validateDuplicateNumbers(winningNumbers);
         validateWinningNumbersCount(winningNumbers);
         validateWinningNumbersRange(winningNumbers);
-        return winningNumbers;
+
+        // List 로 검증 로직한 후 Set 형태로 안전하게 데이터 이동
+        return new HashSet<>(winningNumbers);
 
 
     }
@@ -29,6 +35,15 @@ public class WinningNumberValidator {
     }
 
     // 2.
+    private static void validateDuplicateNumbers(List<Integer> numbers) {
+        long distinctCount = numbers.stream().distinct().count();
+        if (distinctCount != numbers.size()) {
+            throw new IllegalArgumentException("당첨 번호에 중복된 숫자가 있습니다.");
+        }
+    }
+
+
+    // 3.
     private static void validateWinningNumbersRange(List<Integer> numbers) {
         boolean hasInvalid = numbers.stream()
                 .anyMatch(number -> number < 1 || number >= 45);
@@ -38,7 +53,7 @@ public class WinningNumberValidator {
 
     }
 
-    // 3.
+    // 4.
     private static List<Integer> parseNumbers(String input) {
         try {
             return Arrays.stream(input.split(","))
@@ -48,8 +63,6 @@ public class WinningNumberValidator {
         } catch (NumberFormatException e) {
             throw new IllegalArgumentException("숫자만 입력이 가능합니다.");
         }
-
-        // 추가로 개수 6개랑, 1과 45 사이의 숫자 검증 필요
     }
 
 }
--- src/main/java/lotto/view/input/WinningNumberInputView.java
+++ src/main/java/lotto/view/input/WinningNumberInputView.java
@@ -1,12 +1,12 @@
 package lotto.view.input;
 
 import camp.nextstep.edu.missionutils.Console;
-import java.util.List;
+import java.util.Set;
 import lotto.utils.validator.WinningNumberValidator;
 
 public class WinningNumberInputView {
 
-    public static List<Integer> inputWinningNumber() {
+    public static Set<Integer> inputWinningNumber() {
         System.out.println("\n당첨 번호를 입력해 주세요");
         String winningNumber = Console.readLine();
         return WinningNumberValidator.validateInputWinningNumber(winningNumber);
